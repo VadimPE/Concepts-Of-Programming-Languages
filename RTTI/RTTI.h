61c62ab038c43c26566b457479c29fb4ba781255
Vadim <plahtinskiy.ve@phystech.edu>
2018-04-29 19:38:28 +0300

    update

--- RTTI/RTTI.h
+++ RTTI/RTTI.h
@@ -4,19 +4,34 @@
 #include <unordered_set>
 #include <unordered_map>
 
-#define CLASSES std::unordered_map<std::string, std::set<std::string>> classes;
+std::unordered_map<std::string, std::unordered_set<std::string>> classes;
 
 size_t GetHash(std::string class_name) {
     std::hash<std::string> hash_foo;
     return hash_foo(class_name);
-}
+};
 
 template <typename to_class_ptr, typename from_class_ptr>
 to_class_ptr CastForced(from_class_ptr obj_ptr) {
     from_class_ptr* new_obj_ptr = reinterpret_cast<from_class_ptr>(obj_ptr);
     new_obj_ptr->name = obj_ptr->name;
     return new_obj_ptr;
-}
+};
+
+std::unordered_set<std::string> Split(std::string str, char chr) {
+    std::unordered_set<std::string> ans;
+    std::string new_class_name = "";
+    for (auto i = 0; i < str.length(); ++i) {
+        if (str[i] == chr) {
+            ans.insert(new_class_name);
+            new_class_name = "";
+        } else {
+            new_class_name += str[i];
+        }
+    }
+    ans.insert(new_class_name);
+    return ans;
+};
 
 struct type_info {
     type_info(std::string name): name_(name), hash_(GetHash(name)) {};
@@ -29,14 +44,39 @@ struct type_info {
 
 #define BASE_CLASS(class_name) class class_name { \
   public: \
-    class_name() {\
-        classes.emplace(#class_name, class_name::parents_); \
+    class_name(): name_(#class_name) {\
     }; \
-std::string GetInfo() { \
+    static void DECLARE() { \
+        classes.emplace(#class_name, class_name::parents_); \
+    } \
+type_info GetInfo() { \
     return class_name::type_info_; \
 } \
   private: \
     static std::unordered_set<std::string> parents_; \
-    static type_info type_info; \
-    static std::string name_; \
-}; \
+    static type_info type_info_; \
+    std::string name_; \
+  public: \
+
+#define DERIVED_CLASS(class_name, ...) class class_name: __VA_ARGS__ { \
+  public: \
+    class_name(): name_(#class_name) {\
+    }; \
+    static void DECLARE() { \
+        class_name::parents_ = Split(#__VA_ARGS__, ','); \
+        classes.emplace(#class_name, class_name::parents_); \
+    } \
+    type_info GetInfo() { \
+        return class_name::type_info_; \
+    } \
+  private: \
+    static std::unordered_set<std::string> parents_; \
+    static type_info type_info_; \
+    std::string name_; \
+  public: \
+
+#define END_CLASS(class_name) };\
+std::unordered_set<std::string> class_name::parents_; \
+type_info class_name::type_info_ = type_info(#class_name); \
+
+#define DYNAMIC_CAST()
--- RTTI/main.cpp
+++ RTTI/main.cpp
@@ -1,5 +1,25 @@
 #include "RTTI.h"
 
+BASE_CLASS(A)
+int a = 5;
+void foo() {
+    std::cout << a;
+};
+END_CLASS(A)
+
+BASE_CLASS(C)
+int b = 10;
+void foo1() {
+    std::cout << b;
+};
+END_CLASS(C)
+
+DERIVED_CLASS(B, public A, public C)
+END_CLASS(B)
+
 int main() {
+    B::DECLARE();
+    B a;
+    std::cout << a.b;
     return 0;
 }
